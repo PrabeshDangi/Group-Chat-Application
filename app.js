f2beeec87efaf12fc19190f059a06ddb66ab3dde
Prabesh Dangi <dangiprabesh58@gmail.com>
2024-01-12 00:00:36 +0545

    Socket setup

--- app.js
+++ app.js
@@ -6,4 +6,24 @@ const port=process.env.port||5001;
 const server=app.listen(port,()=>{
     console.log(`Server started on port ${port}!!`)
 })
+
+const io=require("socket.io")(server);
+
 app.use(express.static(path.join(__dirname,'public')))
+
+let socketsConnected=new Set()
+
+io.on('connection',onConnected)
+
+function onConnected(socket){
+    console.log(socket.id)
+    socketsConnected.add(socket.id)
+
+    io.emit('clients-total',socketsConnected.size)
+
+    socket.on('disconnect',()=>{
+        console.log("socket disconnected",socket.id);
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total',socketsConnected.size)
+    })
+}
